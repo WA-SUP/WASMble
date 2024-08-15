f2ea84eabe4a1a3dc2cf15cbb1f05b239de49e56
JongBaaam <JongBaaam@gmail.com>
2024-08-16 00:29:05 +0900

    Feat: Main 레이아웃 UI 구현

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+import logo from "@public/wasmble.png";
+
+export default function Header() {
+  return (
+    <>
+      <header className="mx-6 py-3 border-b border-slate-200/10">
+        <div>
+          <Image src={logo} alt="Wasmble Logo Image" width={200} />
+        </div>
+      </header>
+    </>
+  );
+}
