9124afbbfd21b557c62bdad83c5e8d3e158cbd08
JongBaaam <JongBaaam@gmail.com>
2024-08-16 21:49:46 +0900

    Refactor: 헤더 UI 코드 리뷰 반영

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,14 +1,10 @@
-import Image from "next/image";
-
-import logo from "@public/wasmble.png";
+import Logo from "./Logo";
 
 export default function Header() {
   return (
     <>
-      <header className="mx-6 py-3 border-b border-slate-200/10">
-        <div>
-          <Image src={logo} alt="Wasmble Logo Image" width={200} priority />
-        </div>
+      <header className="flex items-center mx-6 py-3 border-b border-slate-200/10">
+        <Logo />
       </header>
     </>
   );
