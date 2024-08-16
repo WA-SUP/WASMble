89bb8c55c134cc13b896fbca7c1a142f14292617
Heestolee <dlckdgml25@gmail.com>
2024-08-17 01:37:10 +0900

    Feat: 입력 코드 AST 파싱 유틸함수 구현

---
+++ src/utils/jsToAstParser.js
@@ -0,0 +1,18 @@
+import * as parser from "@babel/parser";
+
+export default function parseJscodeToAST(request) {
+  const { functionCode } = request.body;
+
+  try {
+    const ast = parser.parse(functionCode, {
+      sourceType: "script",
+    });
+
+    return ast;
+  } catch (error) {
+    return {
+      message: "지원하지 않는 변수의 타입입니다.",
+      errorStackMessage: error.message,
+    };
+  }
+}
