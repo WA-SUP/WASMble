b33edfc230da2aa68370b5849d9d5178aed67f65
Heestolee <dlckdgml25@gmail.com>
2024-08-17 13:48:25 +0900

    Test: jsToAstParser 에러케이스 추가

--- src/utils/jsToAstParser.test.js
+++ src/utils/jsToAstParser.test.js
@@ -1,7 +1,7 @@
 import parseJscodeToAST from "./jsToAstParser";
 
 describe("parseJscodeToAST", () => {
-  it("유효한 함수 코드로 AST 트리가 성공적으로 생성되어야 합니다.", () => {
+  it("유효한 함수 코드로 AST 트리가 성공적으로 생성되어야 한다.", () => {
     const request = {
       body: {
         functionCode: "function add(a, b) { return a + b; }",
@@ -13,5 +13,17 @@ describe("parseJscodeToAST", () => {
     expect(result).toHaveProperty("type", "File");
     expect(result).toHaveProperty("program");
     expect(result).toHaveProperty("start");
+  });
+
+  it("유효하지 않은 함수 코드의 경우 적절한 오류 메시지가 반환되어야 한다.", () => {
+    const request = {
+      body: {
+        functionCode: "function add(a, b) { return a + ; }",
+      },
+    };
+
+    const result = parseJscodeToAST(request);
+
+    expect(result).toHaveProperty("errorStackMessage");
   });
 });
