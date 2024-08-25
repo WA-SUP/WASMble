5a4a31ae8375834cd9f8d789ef5128fc8d28764f
JongBaaam <JongBaaam@gmail.com>
2024-08-25 22:51:01 +0900

    Test: calculateAverageExecutionTime

--- src/logic/performance-comparison/performanceAnalysis.test.js
+++ src/logic/performance-comparison/performanceAnalysis.test.js
@@ -1,4 +1,7 @@
-import { measurePerformanceByFunc } from "./performanceAnalysis";
+import {
+  calculateAverageExecutionTime,
+  measurePerformanceByFunc,
+} from "./performanceAnalysis";
 
 describe("measurePerformanceByFunc", () => {
   function add(a, b) {
@@ -27,5 +30,50 @@ describe("measurePerformanceByFunc", () => {
         type: expect.any(String),
       }),
     );
+  });
+});
+
+describe("calculateAverageExecutionTime", () => {
+  const mockPerformanceResults = new Array(5).fill(0).map((_, index) => {
+    return {
+      cpuUsage: 0,
+      executionTime: null,
+    };
+  });
+
+  it("성능 측정 결과를 순회하며 평균 실행 시간를 반환해야합니다.", () => {
+    expect(calculateAverageExecutionTime(mockPerformanceResults)).toEqual(
+      expect.any(Number),
+    );
+
+    expect(calculateAverageExecutionTime(mockPerformanceResults)).toBe(0);
+  });
+
+  it("실행 시간이 존재하지 않을 경우 0으로 대체하여 결과값을 반환해야합니다.", () => {
+    expect(
+      calculateAverageExecutionTime([
+        {
+          cpuUsage: 0,
+          executionTime: null,
+        },
+        {
+          cpuUsage: 0,
+          executionTime: 1,
+        },
+      ]),
+    ).toEqual(expect.any(Number));
+
+    expect(
+      calculateAverageExecutionTime([
+        {
+          cpuUsage: 0,
+          executionTime: null,
+        },
+        {
+          cpuUsage: 0,
+          executionTime: 1,
+        },
+      ]),
+    ).toBe(1 / 2);
   });
 });
