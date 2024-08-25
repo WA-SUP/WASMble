1bb6bffa5583251f4d64f7ed92d8f8c31297035a
JongBaaam <JongBaaam@gmail.com>
2024-08-25 22:52:23 +0900

    Refactor: wasm 인스턴스 생성 로직 수정

--- src/utils/webAssemblyUtils.js
+++ src/utils/webAssemblyUtils.js
@@ -1,10 +1,5 @@
-import asc from "assemblyscript/dist/asc.js";
+export async function createWasmInstance(wasmBuffer) {
+  const wasmInstance = await WebAssembly.instantiate(wasmBuffer);
 
-export async function createWasmInstance(stringCode) {
-  const { binary: wasmBuffer } = await asc.compileString(stringCode, {
-    optimize: true,
-  });
-  const wasmModule = await WebAssembly.instantiate(wasmBuffer);
-
-  return { ...wasmModule.instance.exports };
+  return wasmInstance.instance.exports;
 }
