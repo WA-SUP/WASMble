2f982682a3c43bcabefbb9bafcd8c5032960fd31
YS <allansad12@gmail.com>
2024-08-26 18:31:18 +0900

    Feat: API POST핸들러 작성

--- src/app/api/performance-comparison/route.js
+++ src/app/api/performance-comparison/route.js
@@ -1,21 +1,70 @@
 import { NextResponse } from "next/server";
+import crypto from "crypto";
+import { readFile } from "fs/promises";
+
+import generate from "@babel/generator";
+import tmp from "tmp";
 
 import executeUserCode from "@logic/performance-comparison/userCodeExecutor";
-
-// 테스트를 위해 작성하였습니다. 성능 비교분석 로직 구현 이후 수정될 예정입니다.
+import parseJscodeToAST from "@utils/jsToAstParser";
+import validateArgsLength from "@logic/ast-analysis/validateArgsLength";
+import assignFunctionTypes from "@logic/ast-analysis/typeInference";
+import * as buildAndCleanup from "@logic/file-operations/buildAndCleanup";
+import executeMeasurementInVm from "@logic/performance-comparison/measurementExecutor";
 
 export async function POST(request) {
-  const userResult = await executeUserCode(request);
+  const UUID = crypto.randomUUID();
+  const tempDirectory = tmp.dirSync({ unsafeCleanup: true });
 
-  if (hasRequestError(userResult)) {
-    return NextResponse.json(userResult, { status: 400 });
+  try {
+    const { functionCode, functionCall, functionArguments, functionName } =
+      await request.json();
+    const parsedFunctionArguments = JSON.parse(functionArguments);
+
+    const userCodeResult = await executeUserCode(functionCode, functionCall);
+
+    const jsAst = parseJscodeToAST(functionCode);
+
+    validateArgsLength(jsAst, parsedFunctionArguments);
+
+    const asAst = assignFunctionTypes(
+      jsAst,
+      parsedFunctionArguments,
+      userCodeResult,
+    );
+    const { code: asCode } = generate.default(asAst, {}, functionCode);
+
+    const asFilePath = await buildAndCleanup.createAsModule(
+      asCode,
+      UUID,
+      tempDirectory,
+    );
+    const wasmFilePath = await buildAndCleanup.createWASM(
+      asFilePath,
+      UUID,
+      tempDirectory,
+    );
+
+    const wasmBuffer = await readFile(wasmFilePath);
+
+    const result = await executeMeasurementInVm({
+      javascriptCode: functionCode,
+      wasmBuffer,
+      extractedJsFuncName: functionName,
+      args: parsedFunctionArguments,
+    });
+
+    buildAndCleanup.deleteTempDirectory(tempDirectory);
+
+    return NextResponse.json({ result, asCode }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+
+    buildAndCleanup.deleteTempDirectory(tempDirectory);
+
+    return NextResponse.json(
+      { errorMessage: "올바르지 않은 요청입니다." },
+      { status: 400 },
+    );
   }
-}
-
-function hasRequestError(userResult) {
-  if (typeof userResult === "object") {
-    return Object.hasOwn(userResult, "errorMessage");
-  }
-
-  return false;
 }
