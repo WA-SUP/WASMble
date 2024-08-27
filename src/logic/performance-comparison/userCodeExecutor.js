89a248cf674cf839dc85f7e879a4f0ec7181fa9b
YS <allansad12@gmail.com>
2024-08-27 15:00:43 +0900

    Refactor: userCodeExecutor 에러핸들링

--- src/logic/performance-comparison/userCodeExecutor.js
+++ src/logic/performance-comparison/userCodeExecutor.js
@@ -1,11 +1,12 @@
 import executeVmCode from "@utils/vmCodeExecutor";
+import ApiError from "@logic/api-error/performanceComparison";
 
 export default async function executeUserCode(functionCode, functionArguments) {
-  const userResult = await executeVmCode(functionCode, functionArguments);
-
   if (!functionCode || !functionArguments) {
-    return { errorMessage: "올바르지 않은 요청입니다." };
+    throw new ApiError("올바르지 않은 요청입니다.", 400);
   }
+
+  const userResult = await executeVmCode(functionCode, functionArguments);
 
   return userResult;
 }
