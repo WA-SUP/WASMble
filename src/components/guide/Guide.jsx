014aee100c4820381880d6c6d753c4a0a344b804
Heestolee <dlckdgml25@gmail.com>
2024-08-28 14:17:32 +0900

    Feat: 가이드UI 구현

--- src/components/guide/Guide.jsx
+++ src/components/guide/Guide.jsx
@@ -1,29 +1,54 @@
 import ContentBox from "@components/common/ContentBox";
 import DiffEditor from "@components/editor/DiffEditor";
-import GuideText from "@components/guide/GuideText";
+import SpeedReport from "@/components/visualization/SpeedReport";
+import PerformanceComparisonChart from "@/components/visualization/PerformanceComparisonChart";
+
+import { guideMockData } from "@/constants/constant";
 
 export default function Guide() {
   return (
-    <div className="flex flex-col p-3 h-full w-full">
-      <div>
-        <h1 className="text-3xl font-bold">가이드</h1>
-        <GuideText />
+    <div className="flex flex-col justify-between p-3 w-full h-full">
+      <div className="flex flex-row justify-between h-48 pb-3">
+        <ContentBox
+          width="w-[48%]"
+          borderColor="border-notice-border"
+          bg="bg-color-notice"
+        >
+          <h2 className="flex items-center font- text-xl">
+            <span className="text-2xl mr-2">&#9888;</span>
+            Notice!
+          </h2>
+          <ul className="mt-2 ml-6 list-disc">
+            <li>데이터 타입은 원시값(Number, String)만 지원합니다.</li>
+            <li>입력한 함수와 동일한 갯수의 매개변수를 입력해주세요.</li>
+          </ul>
+        </ContentBox>
+        <ContentBox width="w-[48%]">
+          <h2 className="flex items-center font-bold text-xl">
+            <span className="text-2xl mr-2">&#128193;</span>
+            Archive
+          </h2>
+        </ContentBox>
       </div>
-      <div>
-        <h2 className="text-2xl font-bold">결과물 예시</h2>
-        <div className="flex flex-col justify-around gap-4 mt-4">
-          <ContentBox>
-            <DiffEditor />
+      <ContentBox custom="flex flex-col justify-around items-center gap-2">
+        <DiffEditor />
+        <div className="flex flex-row justify-around w-full h-full pt-3">
+          <ContentBox
+            width="w-[64%]"
+            height="inherit"
+            custom="flex flex-col justify-center"
+          >
+            <PerformanceComparisonChart data={guideMockData} />
           </ContentBox>
-          <ContentBox>
-            <img
-              src="gauge.png"
-              alt="WebAssembly Performance"
-              className="h-auto mx-auto"
-            />
+          <ContentBox
+            width="w-[32%]"
+            height="inherit"
+            custom="flex flex-col justify-center"
+          >
+            <SpeedReport data={guideMockData} />
           </ContentBox>
         </div>
-      </div>
+      </ContentBox>
     </div>
   );
 }
--- src/constants/constant.js
+++ src/constants/constant.js
@@ -3,3 +3,20 @@ export const CODE_EDITOR_DEFAULT_VALUE = `
     // your code...
   }
 `;
+
+export const guideMockData = {
+  jsCode: "function yourFunction(a, b) { return a + b; }",
+  transpiledAsCode:
+    "function yourFunction(a: number, b: number): number { return a + b; }",
+  operationTimesPerSecond: [
+    {
+      type: "js",
+      operationTimes: 142782,
+    },
+    {
+      type: "wasm",
+      operationTimes: 582742,
+    },
+  ],
+  performanceReportId: "guide-sample",
+};
